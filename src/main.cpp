863228d818c2559dd370d1a1f93ee950328db1ca
hamuchan214 <hamuchan214@gmail.com>
2023-09-04 23:27:49 +0900

    add simple CAN sender code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <CAN.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  delay(100);
+  Serial.begin(115200);
+  CAN.setPins(33,27);
+  if(!CAN.begin(1000E3))
+  {
+    Serial.println("CAN start failed!");
+    while (1);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+// send packet: id is 11 bits, packet can contain up to 8 bytes of data
+  Serial.print("Sending packet ... ");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  CAN.beginPacket(0x12);
+  CAN.write('h');
+  CAN.write('e');
+  CAN.write('l');
+  CAN.write('l');
+  CAN.write('o');
+  CAN.endPacket();
+
+  Serial.println("done");
+
+  delay(1000);
 }
